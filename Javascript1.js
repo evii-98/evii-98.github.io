fd296c405df2cd48cfbf65dc8de0b7037a99f8f0
Evelin <32090022+evii-98@users.noreply.github.com>
2018-02-19 17:11:34 -0800

    Add files via upload

--- Javascript1.js
+++ Javascript1.js
@@ -1,17 +1,12 @@
-/*Variables
-function openingWebPrompt() {
-  var guessName = prompt ('Hello, and you are?');
-  alert ("hello " + guessName);
-}
-/*var guessAdress= "";
-var guessBody="";
-var guessSubject="";
-
-document.write (
-  Please fill out
-);
-*/
+/*Learning how to change images/ slideshow
 function changingImages() {
 var homeImages= document.getElementById('img1').id;
 }
 <button onlick ="changingImages()"> next picture </button>
+*/
+// Search Engine
+var search;
+var stock;
+var inStock= ['unicorn cupcpcakes' + stock];
+
+function 
